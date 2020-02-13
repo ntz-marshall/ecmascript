5693ff9160c14f9a2f3a1771c5868895a865992c
Marcelle Nhemetz <swolv.lith@gmail.com>
2020-02-13 18:30:30 -0300

    Criando um objeto Date

--- aluraframe/client/js/app/controllers/NegotiationsController.js
+++ aluraframe/client/js/app/controllers/NegotiationsController.js
@@ -1,20 +1,25 @@
 class NegotiationsController {
 
     constructor() {
+
         let $ = document.querySelector.bind(document)
         // .bind atrela o querySelector ao document, masmo passando ele para $, fazendo com que essa representação funcione
-        this.inputDate = $("#data")
-        this.inputQtt = $("#quantidade")
-        this.inputValue = $("#valor")
+        this._inputDate = $("#data")
+        this._inputQtt = $("#quantidade")
+        this._inputValue = $("#valor")
     }
   
   
     addict(event) {
 
-        event.preventDefault();
+        event.preventDefault()
 
-        console.log(this.inputDate.value)
-        console.log(this.inputQtt.value)
-        console.log(this.inputValue.value)
+        const date = new Date(this._inputDate.value.replace(/-/g, ',')) // exp reg global que troca o - por ,
+
+       let negotiations = new Negotiations(
+           date,
+            this._inputQtt.value,
+            this._inputValue.value)
+            console.log(negotiations)
     }
 }
