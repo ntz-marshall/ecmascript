d6104dd8c7f80ea59389a8329072192a25a594e6
Marcelle Nhemetz <swolv.lith@gmail.com>
2020-02-13 17:34:03 -0300

    Adicionando construrto, erro de inicializaãço resolvido

--- aluraframe/client/js/app/controllers/NegotiationsController.js
+++ aluraframe/client/js/app/controllers/NegotiationsController.js
@@ -1,7 +1,20 @@
 class NegotiationsController {
 
+    constructor() {
+        let $ = document.querySelector.bind(document)
+        // .bind atrela o querySelector ao document, masmo passando ele para $, fazendo com que essa representação funcione
+        this.inputDate = $("#data")
+        this.inputQtt = $("#quantidade")
+        this.inputValue = $("#valor")
+    }
+  
+  
     addict(event) {
-        event.preventDefault()
-        alert("Chamei ação no controller")
+
+        event.preventDefault();
+
+        console.log(this.inputDate.value)
+        console.log(this.inputQtt.value)
+        console.log(this.inputValue.value)
     }
 }
