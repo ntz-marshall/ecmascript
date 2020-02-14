f9d64ed7e96b4768067129b6c0cbb742ac0a717d
Marcelle Nhemetz <swolv.lith@gmail.com>
2020-02-14 03:25:44 -0300

    Exercício 8 validação de código

--- aluraframe/client/js/ex.js
+++ aluraframe/client/js/ex.js
@@ -99,3 +99,28 @@
         .map(function(prova) { return prova.aluno.nome;}) 
 
         console.log(aprovados)
+
+        /* ~ RegEx ~ */
+        // fazer uma classe
+        function validaCodigo(codigo) {
+
+            // cria a expressão regular. Poderíamos ter usado 
+            // a sintaxe new RegExp(/\D{3}-\D{2}-\d{2}/)
+            // \D é qualquer coisa não dígito
+            // \D{3} é qualquer coisa não dígito que forme um grupo de 3 caracteres
+            // \d é qualquer dígito.
+            let expressao = /\D{3}-\D{2}-\d{2}/; 
+        
+            // toda expressão regular possui o método test 
+            // que recebe o alvo do teste, retornando true
+            // se passou, e false se falhou
+            if(expressao.test(codigo)) {
+                  alert('Código válido!');
+              } else {
+                  alert('Código inválido');
+              }
+        
+        }
+        
+        validaCodigo('GWZ-JJ-12'); // válido
+        validaCodigo('1X1-JJ-12'); // inválido
