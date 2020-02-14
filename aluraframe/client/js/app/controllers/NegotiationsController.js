15010ec0360c19137ff64be0245a18a21d43d652
Marcelle Nhemetz <swolv.lith@gmail.com>
2020-02-14 02:34:39 -0300

    Implementando DateHelper, usando exp reg

--- aluraframe/client/js/app/controllers/NegotiationsController.js
+++ aluraframe/client/js/app/controllers/NegotiationsController.js
@@ -11,17 +11,12 @@ class NegotiationsController {
 	
 		addict(event) {
 
-				event.preventDefault()
+			event.preventDefault()
 
-			 //const date = new Date(this._inputDate.value.replace(/-/g, ',')) exp reg global que troca o - por ,
-			 // mesmo usando apenas a exp reg não havia erro de data.
-			 let date = new Date(...this._inputDate.value.split('-').map((item, indice) => item - indice % 2 )) // arrow function já faz o return e se for apenas uma linha pode-se omitir o bloco
-						// if (indice == 1) {return item -1}
-						// indice 0 % 2 é 0 então não há subtração, indice 1 % 2 é 1 então há subtração, indice 2 % 2 é 0.
-			 let negotiations = new Negotiations(
-					 date,
-						this._inputQtt.value,
-						this._inputValue.value)
-						console.log(negotiations)
+			// Chamando diretamente na classe o método
+			let negotiations = new Negotiations(DateHelper.stringToDate(this._inputDate.value),	this._inputQtt.value, this._inputValue.value)
+			
+			console.log(negotiations)
+			console.log(DateHelper.dateToString(negotiations.data))
 		}
 }
