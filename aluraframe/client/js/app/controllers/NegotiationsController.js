d475382ecda92d98607ae614c3e56154da1a554f
Marcelle Nhemetz <swolv.lith@gmail.com>
2020-02-13 23:36:11 -0300

    Terminando aula 3

--- aluraframe/client/js/app/controllers/NegotiationsController.js
+++ aluraframe/client/js/app/controllers/NegotiationsController.js
@@ -9,15 +9,13 @@ class NegotiationsController {
 				this._inputValue = $("#valor")
 		}
 	
-	
 		addict(event) {
 
 				event.preventDefault()
 
 			 //const date = new Date(this._inputDate.value.replace(/-/g, ',')) exp reg global que troca o - por ,
 			 // mesmo usando apenas a exp reg não havia erro de data.
-			 let date = new Date(...
-				this._inputDate.value.split('-').map((item, indice) => item - indice % 2 )) // arrow function já faz o return e se for apenas uma linha pode-se omitir o bloco
+			 let date = new Date(...this._inputDate.value.split('-').map((item, indice) => item - indice % 2 )) // arrow function já faz o return e se for apenas uma linha pode-se omitir o bloco
 						// if (indice == 1) {return item -1}
 						// indice 0 % 2 é 0 então não há subtração, indice 1 % 2 é 1 então há subtração, indice 2 % 2 é 0.
 			 let negotiations = new Negotiations(
